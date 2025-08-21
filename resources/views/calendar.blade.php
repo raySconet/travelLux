24793f1a500643133ba3d4ecd09189c5a8930f92
Rony Chammai <rchammai@sconet.net>
2025-08-21 16:15:12 +0300

    Calendar update

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -7,38 +7,40 @@
         </h2>
     </x-slot>
 
-    <div class="text-gray-900 px-5 py-7 grid grid-cols-1 xl:grid-cols-12 gap-2">
-        <div class="xl:col-span-3 flex flex-col h-full">
+    <div class="text-gray-900 px-5 py-7 grid grid-cols-1 2xl:grid-cols-12 gap-2">
+        <div class="2xl:col-span-3 flex flex-col h-full">
             <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="px-6 pb-4 border-b flex items-center justify-between">
+                    <div class="pb-4 border-b flex items-center justify-between">
                         <h2 id="sidebarCalendarMonthYearSelected" class="text-lg font-semibold">{{ date('F Y') }}</h2>
                         <div class="space-x-2">
-                            <button class="text-gray-600 hover:text-black cursor-pointer">&larr;</button>
-                            <button class="text-gray-600 hover:text-black cursor-pointer">&rarr;</button>
+                            <button id="sidebarCalendarPrevMonth" class="text-gray-600 hover:text-black cursor-pointer">&larr;</button>
+                            <button id ="sidebarCalendarNextMonth" class="text-gray-600 hover:text-black cursor-pointer">&rarr;</button>
                         </div>
                     </div>
 
                     @include('calendar.calendar-sidebar') {{-- Include the calendar sidebar component --}}
                 </div>
             </div>
-            <div class="flex-1 bg-white overflow-hidden shadow-xs sm:rounded-lg mt-2">
-                <div class="p-6">
+            <div class=" bg-white overflow-visible shadow-xs sm:rounded-lg mt-2 p-6 h-full">
+                <div class="">
                     @include('calendar.lawyers-sidebar') {{-- Include the calendar sidebar component --}}
                 </div>
             </div>
         </div>
-        <div class="xl:col-span-9 mx-auto flex flex-col h-full">
-            <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                <div class="p-6">
-                    @include('calendar.calendar-header') {{-- Include the calendar header component --}}
+        <div class="2xl:col-span-9 flex flex-col h-full overflow-x-auto">
+            <div class="min-w-[1075px] flex flex-col h-full">
+                <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
+                    <div class="p-6">
+                        @include('calendar.calendar-header') {{-- Include the calendar header component --}}
+                    </div>
                 </div>
-            </div>
 
-            <div class="mt-2 flex-1 flex flex-col">
-                <div class="flex-1 bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                    <div class="p-6 h-full">
-                        @include('calendar.draw-calendar') {{-- Include the calendar drawing component --}}
+                <div class="mt-2 flex-1 flex flex-col">
+                    <div class="flex-1 bg-white xl:overflow-visible overflow-hidden shadow-xs sm:rounded-lg">
+                        <div class="p-6 h-full w-full">
+                            @include('calendar.draw-calendar') {{-- Include the calendar drawing component --}}
+                        </div>
                     </div>
                 </div>
             </div>

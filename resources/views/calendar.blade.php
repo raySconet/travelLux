06bafced7516d96dc35616651968117894a9c6b7
unknown <rjaoudeh@sconet.net>
2025-08-21 16:40:35 +0300

    aa

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,7 +1,7 @@
 @vite('resources/css/calendar.css')
 
 <x-app-layout>
-    <x-slot name="header">
+    <x-slot name="header" >
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Calendar') }}
         </h2>

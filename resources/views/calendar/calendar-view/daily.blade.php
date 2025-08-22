ba400a06f986482b66383adbe69343bdc1cfd26b
Rony Chammai <rchammai@sconet.net>
2025-08-22 14:47:59 +0300

    Calendar update

--- resources/views/calendar/calendar-view/daily.blade.php
+++ resources/views/calendar/calendar-view/daily.blade.php
@@ -1,41 +1,39 @@
-<div class="grid grid-cols-2 gap-4 w-full">
-    <table class="w-full bg-[#eaf1ff] border border-[#fff]">
-        <thead>
-            <tr>
-                <x-calendar-components.th class="p-4 text-start border-0 flex justify-between">
-                    Sunday
-                    <div>~John Doe</div>
-                </x-calendar-components.th>
-            </tr>
-        </thead>
-        <tbody class="flex flex-col border-t-2 border-[#fff]">
-            <tr class="calendarRowData">
-                <x-calendar-components.td class="h-full px-4 border-0">
-                    <div class="text-gray-900 dailyEventInfo bg-[#30d80fb3]" draggable="true">
-                        <span>AAM - NEW REFERRAL - SIMONE ALEXANDER</span>
-                    </div>
-                </x-calendar-components.td>
-            </tr>
-        </tbody>
-    </table>
+<table class="w-full bg-[#eaf1ff] border border-[#fff]">
+    <thead>
+        <tr>
+            <x-calendar-components.th class="p-4 text-start border-0 flex justify-between">
+                Sunday
+                <div>~John Doe</div>
+            </x-calendar-components.th>
+        </tr>
+    </thead>
+    <tbody class="flex flex-col border-t-2 border-[#fff]">
+        <tr class="calendarRowData">
+            <x-calendar-components.td class="h-full px-4 border-0">
+                <div class="text-gray-900 dailyEventInfo bg-[#30d80fb3]" draggable="true">
+                    <span>AAM - NEW REFERRAL - SIMONE ALEXANDER</span>
+                </div>
+            </x-calendar-components.td>
+        </tr>
+    </tbody>
+</table>
 
-    <table class="w-full bg-[#eaf1ff] border border-[#fff]">
-        <thead>
-            <tr>
-                <x-calendar-components.th class="p-4 text-start border-0 flex justify-between">
-                    Sunday
-                    <div>~Jane Smith</div>
-                </x-calendar-components.th>
-            </tr>
-        </thead>
-        <tbody class="flex flex-col border-t-2 border-[#fff]">
-            <tr class="calendarRowData">
-                <x-calendar-components.td class="h-full px-4 border-0">
-                    <div class="text-gray-900 dailyEventInfo bg-[#30d80fb3]" draggable="true">
-                        <span>AAM - NEW REFERRAL - JANE SMITH</span>
-                    </div>
-                </x-calendar-components.td>
-            </tr>
-        </tbody>
-    </table>
-</div>
+<table class="w-full bg-[#eaf1ff] border border-[#fff]">
+    <thead>
+        <tr>
+            <x-calendar-components.th class="p-4 text-start border-0 flex justify-between">
+                Sunday
+                <div>~Jane Smith</div>
+            </x-calendar-components.th>
+        </tr>
+    </thead>
+    <tbody class="flex flex-col border-t-2 border-[#fff]">
+        <tr class="calendarRowData">
+            <x-calendar-components.td class="h-full px-4 border-0">
+                <div class="text-gray-900 dailyEventInfo bg-[#30d80fb3]" draggable="true">
+                    <span>AAM - NEW REFERRAL - JANE SMITH</span>
+                </div>
+            </x-calendar-components.td>
+        </tr>
+    </tbody>
+</table>

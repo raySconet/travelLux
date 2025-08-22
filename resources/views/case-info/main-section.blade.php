187510991fb69b9511e96bf63328b608197559f0
unknown <rjaoudeh@sconet.net>
2025-08-22 18:38:17 +0300

    aa

---
+++ resources/views/case-info/main-section.blade.php
@@ -0,0 +1,174 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900">
+            {{ __('Contact Info') }}
+        </h2>
+
+        {{-- <p class="mt-1 text-sm text-gray-600">
+            {{ __("Update your account's profile information and email address.") }}
+        </p> --}}
+    </header>
+
+    <form method="post" action="{{ route('profile.update') }}" class="mt-3">
+        @csrf
+        <div class="flex mt-[-40px] float-right">
+            <x-primary-btn class="">{{ __('Save') }}</x-primary-btn>
+
+            @if (session('status') === 'profile-updated')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600"
+                >{{ __('Saved.') }}</p>
+            @endif
+        </div>
+        <div class="grid grid-cols-1 2xl:grid-cols-12 gap-4 w-full">
+            <div class="2xl:col-span-3 flex flex-col ">
+                <p class="mb-1 text-md text-gray-600">
+                    {{ __("Client") }}
+                </p>
+                <div >
+                    <x-input-label for="tel" :value="__('tel')" />
+                    <x-text-input id="tel" name="tel" type="text" class="mt-1" :value="old('name')"  required autofocus autocomplete="tel" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Email" :value="__('Email')" />
+                    <x-text-input id="Email" name="Email" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Email" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Address" :value="__('Address')" />
+                    <x-text-input id="Address" name="Address" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Address" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="DOB" :value="__('DOB')" />
+                    <x-text-input id="DOB" name="DOB" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="DOB" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="SS#" :value="__('SS#')" />
+                    <x-text-input id="SS#" name="SS#" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="SS#" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+            </div>
+
+            <div class="2xl:col-span-3 flex flex-col ">
+                <p class="mb-1 text-md text-gray-600">
+                    {{ __("3P") }}
+                </p>
+                <div>
+                    <x-input-label for="tel" :value="__('tel')" />
+                    <x-text-input id="tel" name="tel" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="tel" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Email" :value="__('Email')" />
+                    <x-text-input id="Email" name="Email" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Email" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Address" :value="__('Address')" />
+                    <x-text-input id="Address" name="Address" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Address" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="DOB" :value="__('DOB')" />
+                    <x-text-input id="DOB" name="DOB" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="DOB" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="SS#" :value="__('SS#')" />
+                    <x-text-input id="SS#" name="SS#" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="SS#" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+            </div>
+
+            <div class="2xl:col-span-3 flex flex-col ">
+                <p class="mb-1 text-md text-gray-600">
+                    {{ __("1P") }}
+                </p>
+                <div>
+                    <x-input-label for="tel" :value="__('tel')" />
+                    <x-text-input id="tel" name="tel" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="tel" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Email" :value="__('Email')" />
+                    <x-text-input id="Email" name="Email" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Email" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Address" :value="__('Address')" />
+                    <x-text-input id="Address" name="Address" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Address" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="DOB" :value="__('DOB')" />
+                    <x-text-input id="DOB" name="DOB" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="DOB" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="SS#" :value="__('SS#')" />
+                    <x-text-input id="SS#" name="SS#" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="SS#" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+            </div>
+
+            <div class="2xl:col-span-3 flex flex-col ">
+                <p class="mb-1 text-md text-gray-600">
+                    {{ __("Defense Counsel") }}
+                </p>
+                <div>
+                    <x-input-label for="tel" :value="__('tel')" />
+                    <x-text-input id="tel" name="tel" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="tel" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Email" :value="__('Email')" />
+                    <x-text-input id="Email" name="Email" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Email" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                 <div>
+                    <x-input-label for="Address" :value="__('Address')" />
+                    <x-text-input id="Address" name="Address" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="Address" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="DOB" :value="__('DOB')" />
+                    <x-text-input id="DOB" name="DOB" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="DOB" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+
+                <div>
+                    <x-input-label for="SS#" :value="__('SS#')" />
+                    <x-text-input id="SS#" name="SS#" type="text" class="mt-1 " :value="old('name')"  required autofocus autocomplete="SS#" />
+                    <x-input-error class="mt-2" :messages="$errors->get('name')" />
+                </div>
+            </div>
+
+
+        </div>
+
+
+    </form>
+</section>
--- resources/views/caseInfo.blade.php
+++ resources/views/caseInfo.blade.php
@@ -3,32 +3,32 @@
 <x-app-layout>
     <x-slot name="header" >
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Calendar') }}
+            {{ __('Case Info') }}
         </h2>
     </x-slot>
 
     <div class="text-gray-900 px-5 py-5 grid grid-cols-1 2xl:grid-cols-12 gap-2">
-        <div class="2xl:col-span-6 flex flex-col h-full">
+        <div class="2xl:col-span-8 flex flex-col h-full">
             <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                <div class="p-6 text-gray-900">
+                <div class="p-3 text-gray-900">
                     @include('case-info.main-section')
                 </div>
             </div>
         </div>
-        <div class="2xl:col-span-3 flex flex-col h-full overflow-x-auto">
-            <div class="min-w-[1075px] flex flex-col h-full">
+        <div class="2xl:col-span-2 flex flex-col h-full ">
+            <div class=" flex flex-col h-full">
                 <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                    <div class="p-6">
-                        @include('calendar.calendar-header')
+                    <div class="p-2">
+                        {{-- @include('calendar.calendar-header') --}}
                     </div>
                 </div>
             </div>
         </div>
-        <div class="2xl:col-span-3 flex flex-col h-full overflow-x-auto">
-            <div class="min-w-[1075px] flex flex-col h-full">
+        <div class="2xl:col-span-2 flex flex-col h-full ">
+            <div class=" flex flex-col h-full">
                 <div class="bg-white overflow-hidden shadow-xs sm:rounded-lg">
-                    <div class="p-6">
-                        @include('calendar.calendar-header')
+                    <div class="p-2">
+                        {{-- @include('calendar.calendar-header') --}}
                     </div>
                 </div>
             </div>

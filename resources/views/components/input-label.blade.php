199575687616dd30ee32ad975dc1b234f2835dff
unknown <rjaoudeh@sconet.net>
2025-09-10 14:43:21 +0300

    aaa

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -2,6 +2,6 @@
 
 <label
     id="{{ $id }}"
-    {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 ' . $class]) }}>
+    {{ $attributes->merge(['class' => 'block  text-gray-700 ' . $class]) }}>
         {{ $value ?? $slot }}
 </label>

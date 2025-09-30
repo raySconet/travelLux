278513633ed6b57cff4accbea8e035c082075a2f
unknown <rjaoudeh@sconet.net>
2025-09-30 12:02:56 +0300

    aa

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,14 @@ import tailwind from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [
     laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
+      input: ['resources/css/app.css',
+              'resources/css/calendar.css',
+              'resources/css/category.css',
+               'resources/js/app.js',
+               'resources/js/calendar.js',
+               'resources/js/category.js',
+               'resources/js/courtCases.js',
+      ],
       refresh: true,
     }),
     tailwind(),

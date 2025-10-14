ff8266859af81e447be61190e076e7c3b13c6c6d
unknown <rjaoudeh@sconet.net>
2025-10-14 11:09:13 +0300

    a

--- resources/views/components/case-components/grid.blade.php
+++ resources/views/components/case-components/grid.blade.php
@@ -1,5 +1,5 @@
 @props(['class' => ''])
 
-<div {{ $attributes->merge(['class' => 'grid grid-cols-1  gap-2 w-full ' . $class]) }}>
+<div {{ $attributes->merge(['class' => 'grid grid-cols-1  gap-0.5 w-full ' . $class]) }}>
     {{ $slot }}
 </div>

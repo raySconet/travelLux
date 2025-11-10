3a2bc813f2812945026329b6950f5f9d239896c2
Rony Chammai <rchammai@sconet.net>
2025-11-10 11:56:20 +0200

    full calendar test

--- resources/views/calendar/calendar-view.blade.php
+++ resources/views/calendar/calendar-view.blade.php
@@ -43,12 +43,12 @@
                 <div id="viewWeekly" class="grid grid-rows-1 2xl:grid-rows-[40%_60%] gap-px p-6 h-full 2xl:h-[1322px] hidden">
                     @include('calendar.calendar-parts.weekly') {{-- Include the calendar drawing component --}}
                 </div>
-                <div id="viewDaily" class="p-6 hidden h-full overflow-x-auto min-h-[678px]"> {{-- overflow-x-auto min-h-[678px] --}}
-                    <div class="grid grid-cols-12 grid-rows-[auto_1fr] gap-0 h-full min-w-[1161px]"> {{-- min-w-[1161px] --}}
+                <div id="viewDaily" class="p-6 hidden h-full overflow-x-auto min-h-[278px]"> {{-- added: overflow-x-auto remove: min-h-[678px] --}}
+                    <div class="grid grid-cols-12 grid-rows-[auto_1fr] gap-0 h-full xl:min-w-[1161px]"> {{-- added xl, it was min-w-[1161px] --}}
                         <div class="col-span-12 grid grid-cols-12 gap-0 h-full">
                             @include('calendar.calendar-parts.daily')
                         </div>
-                        <div class="col-span-12 grid grid-cols-12 gap-0 h-full">
+                        <div class="col-span-12 grid grid-cols-12 gap-0 h-full max-xl:hidden">
                             <div id="dailyBox3" class="bg-[#eaf1ff] col-span-12 xl:col-span-6 h-full border-x-1 border-[#fff]"></div>
                             <div id="dailyBox4" class="bg-[#eaf1ff] col-span-12 xl:col-span-6 h-full border-x-1 border-[#fff] hidden"></div>
                         </div>

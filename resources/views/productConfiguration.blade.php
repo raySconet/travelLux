f1b784b931c37846ea50fa31585b6f503d2e4d24
unknown <rjaoudeh@sconet.net>
2026-01-22 13:11:45 +0200

    home models

--- resources/views/productConfiguration.blade.php
+++ resources/views/productConfiguration.blade.php
@@ -37,10 +37,5 @@
                     </x-col>
                 </x-grid>
             </div>
-            {{-- <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
-            </div> --}}
         </div>
 </x-app-layout>

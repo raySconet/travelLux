1f50b9fd36a6486e41ff9bf04db1f66ee148327e
unknown <rjaoudeh@sconet.net>
2026-02-10 17:27:22 +0200

    aa

--- app/Http/Controllers/top15ExpensiveTripsReportController.php
+++ app/Http/Controllers/top15ExpensiveTripsReportController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class top15ExpensiveTripsReportController extends Controller
+class Top15ExpensiveTripsReportController extends Controller
 {
     public function index()
     {
